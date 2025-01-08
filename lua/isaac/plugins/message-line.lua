be99d84d9ff6af3b655aacdf79d7f2928b0584ae
Isaac Leong <isaacleong23367@gmail.com>
2025-01-08 15:20:09 +0800

    Added Message Line Plugins

---
+++ lua/isaac/plugins/message-line.lua
@@ -0,0 +1,7 @@
+-- This extension extension is for performing the splitting of the messages
+return {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+        require("lsp_lines").setup()
+    end,
+}
