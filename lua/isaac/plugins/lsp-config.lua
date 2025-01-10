2f5e692e13b34e6370b753692d1e3f17cc0876a0
Isaac Leong <isaacleong23367@gmail.com>
2025-01-10 15:22:27 +0800

    Updated Go to Def Shortcuts

--- lua/isaac/plugins/lsp-config.lua
+++ lua/isaac/plugins/lsp-config.lua
@@ -90,9 +90,9 @@ return {
                 capabilities = capabilities,
             })
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
