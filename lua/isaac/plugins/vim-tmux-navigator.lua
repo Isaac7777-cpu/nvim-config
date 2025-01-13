b1391873d198fd6e2fe7cb6563c18b2d8e4f9f6f
Isaac Leong <isaacleong23367@gmail.com>
2025-01-13 17:29:20 +0800

    Updated keymaps

--- lua/isaac/plugins/vim-tmux-navigator.lua
+++ lua/isaac/plugins/vim-tmux-navigator.lua
@@ -1,10 +1,10 @@
 return {
     {
         "christoomey/vim-tmux-navigator",
-        vim.keymap.set("n", "<c-k><c-h>", ":TmuxNavigateLeft<cr>"),
-        vim.keymap.set("n", "<c-k><c-j>", ":TmuxNavigateDown<cr>"),
-        vim.keymap.set("n", "<c-k><c-k>", ":TmuxNavigateUp<cr>"),
-        vim.keymap.set("n", "<c-k><c-l>", ":TmuxNavigateRight<cr>"),
+        vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<cr>"),
+        vim.keymap.set("n", "<c-j>", ":TmuxNavigateDown<cr>"),
+        vim.keymap.set("n", "<c-k>", ":TmuxNavigateUp<cr>"),
+        vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<cr>"),
         vim.keymap.set("n", "<c-k><c-\\>", ":TmuxNavigatePrevious<cr>"),
     },
 }
