71f7a8d25eebfd824645471736df990406f7c465
Isaac Leong <isaacleong23367@gmail.com>
2025-03-05 18:33:22 +1100

    Updated config for SQLs

--- lua/isaac/plugins/completions.lua
+++ lua/isaac/plugins/completions.lua
@@ -45,8 +45,9 @@ return {
           completeopt = "menu,menuone,noinsert,noselect",
         },
         mapping = {
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
+          -- Disable for keeping the tmux navigator 
+          -- ["<C-k>"] = cmp.mapping.select_prev_item(),
+          -- ["<C-j>"] = cmp.mapping.select_next_item(),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
