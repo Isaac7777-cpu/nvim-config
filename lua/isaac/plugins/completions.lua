90406775f68e7615d909d9d2dba5ad72771300a3
Isaac Leong <isaacleong23367@gmail.com>
2025-04-04 00:55:26 +0800

    Updated 4/4
    Updated 4/4
    
    Test
    
    Test
    
    Reformat

--- lua/isaac/plugins/completions.lua
+++ lua/isaac/plugins/completions.lua
@@ -1,146 +1,146 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-calc",
-      "hrsh7th/cmp-emoji",
-      "saadparwaiz1/cmp_luasnip",
-      "f3fora/cmp-spell",
-      "ray-x/cmp-treesitter",
-      "kdheepak/cmp-latex-symbols",
-      "jmbuhr/cmp-pandoc-references",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-      "onsails/lspkind-nvim",
-      "jmbuhr/otter.nvim",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-calc",
+			"hrsh7th/cmp-emoji",
+			"saadparwaiz1/cmp_luasnip",
+			"f3fora/cmp-spell",
+			"ray-x/cmp-treesitter",
+			"kdheepak/cmp-latex-symbols",
+			"jmbuhr/cmp-pandoc-references",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind-nvim",
+			"jmbuhr/otter.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        preselect = "none",
-        completion = {
-          completeopt = "menu,menuone,noinsert,noselect",
-        },
-        mapping = {
-          -- Disable for keeping the tmux navigator 
-          -- ["<C-k>"] = cmp.mapping.select_prev_item(),
-          -- ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				preselect = "none",
+				completion = {
+					completeopt = "menu,menuone,noinsert,noselect",
+				},
+				mapping = {
+					-- Disable for keeping the tmux navigator
+					-- ["<C-k>"] = cmp.mapping.select_prev_item(),
+					-- ["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
 
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = false,
-          }),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = false,
+					}),
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif require("luasnip").expand_or_jumpable() then
-              require("luasnip").expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif require("luasnip").expand_or_jumpable() then
+							require("luasnip").expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif require("luasnip").jumpable(-1) then
-              require("luasnip").jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        },
-        autocomplete = false,
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif require("luasnip").jumpable(-1) then
+							require("luasnip").jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				},
+				autocomplete = false,
 
-        ---@diagnostic disable-next-line: missing-fields
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol",
-            menu = {
-              otter = "[🦦]",
-              nvim_lsp = "[LSP]",
-              nvim_lsp_signature_help = "[sig]",
-              luasnip = "[snip]",
-              buffer = "[buf]",
-              path = "[path]",
-              spell = "[spell]",
-              pandoc_references = "[ref]",
-              tags = "[tag]",
-              treesitter = "[TS]",
-              calc = "[calc]",
-              latex_symbols = "[tex]",
-              emoji = "[emoji]",
-            },
-          }),
-        },
-        sources = {
-          { name = "otter" }, -- for code chunks in quarto
-          { name = "path" },
-          { name = "nvim_lsp_signature_help" },
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "pandoc_references" },
-          { name = "buffer",                 keyword_length = 3, max_item_count = 20 },
-          { name = "spell" },
-          { name = "treesitter",             keyword_length = 3, max_item_count = 10 },
-          { name = "calc" },
-          { name = "latex_symbols" },
-          { name = "emoji" },
-        },
-        view = {
-          docs = {
-            auto_open = false,
-          },
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-      })
+				---@diagnostic disable-next-line: missing-fields
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						menu = {
+							otter = "[🦦]",
+							nvim_lsp = "[LSP]",
+							nvim_lsp_signature_help = "[sig]",
+							luasnip = "[snip]",
+							buffer = "[buf]",
+							path = "[path]",
+							spell = "[spell]",
+							pandoc_references = "[ref]",
+							tags = "[tag]",
+							treesitter = "[TS]",
+							calc = "[calc]",
+							latex_symbols = "[tex]",
+							emoji = "[emoji]",
+						},
+					}),
+				},
+				sources = {
+					{ name = "otter" }, -- for code chunks in quarto
+					{ name = "path" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "pandoc_references" },
+					{ name = "buffer", keyword_length = 3, max_item_count = 20 },
+					{ name = "spell" },
+					{ name = "treesitter", keyword_length = 3, max_item_count = 10 },
+					{ name = "calc" },
+					{ name = "latex_symbols" },
+					{ name = "emoji" },
+				},
+				view = {
+					docs = {
+						auto_open = false,
+					},
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+			})
 
-      -- for friendly snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-      -- for custom snippets
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snips" } })
-      -- Link quarto and rmarkdown to markdown snippets
-      luasnip.filetype_extend("quarto", { "markdown" })
-      luasnip.filetype_extend("rmarkdown", { "markdown" })
+			-- for friendly snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
+			-- for custom snippets
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snips" } })
+			-- Link quarto and rmarkdown to markdown snippets
+			luasnip.filetype_extend("quarto", { "markdown" })
+			luasnip.filetype_extend("rmarkdown", { "markdown" })
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-    end,
-  },
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
 }
--- lua/isaac/plugins/lsp-config.lua
+++ lua/isaac/plugins/lsp-config.lua
@@ -1,333 +1,350 @@
 return {
-  -- none-ls is a tool that can do automatic installation of the linting source, but also requires lsps
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      -- referenced from http://github.com/milanglacier/nvim/blob/db850bbe400766932c1290c11d1e17672c324cbb/lua/conf/lsp_tools.lua#L135
-      local util = require("null-ls.utils")
-      local helper = require("null-ls.helpers")
+	{
 
-      local function root_pattern_wrapper(patterns)
-        -- referenced from
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/lua/null-ls/builtins/diagnostics/flake8.lua
-        return helper.cache.by_bufnr(function(params)
-          return util.root_pattern(".git", unpack(patterns or {}))(params.bufname)
-        end)
-      end
+		-- for lsp features in code cells / embedded code
+		"jmbuhr/otter.nvim",
+		dev = false,
+		dependencies = {
+			{
+				"neovim/nvim-lspconfig",
+				"nvim-treesitter/nvim-treesitter",
+			},
+		},
+		opts = {
+			verbose = {
+				no_code_found = false,
+			},
+		},
+	},
+	-- none-ls is a tool that can do automatic installation of the linting source, but also requires lsps
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			-- referenced from http://github.com/milanglacier/nvim/blob/db850bbe400766932c1290c11d1e17672c324cbb/lua/conf/lsp_tools.lua#L135
+			local util = require("null-ls.utils")
+			local helper = require("null-ls.helpers")
 
-      local function source_wrapper(args)
-        local source = args[1]
-        local patterns = args[2]
-        args[1] = nil
-        args[2] = nil
-        args.cwd = args.cwd or root_pattern_wrapper(patterns)
-        return source.with(args)
-      end
+			local function root_pattern_wrapper(patterns)
+				-- referenced from
+				-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/lua/null-ls/builtins/diagnostics/flake8.lua
+				return helper.cache.by_bufnr(function(params)
+					return util.root_pattern(".git", unpack(patterns or {}))(params.bufname)
+				end)
+			end
 
-      local sql_formatter_config_file = os.getenv("HOME") .. "/.config/sql_formatter/sql_formatter.json"
+			local function source_wrapper(args)
+				local source = args[1]
+				local patterns = args[2]
+				args[1] = nil
+				args[2] = nil
+				args.cwd = args.cwd or root_pattern_wrapper(patterns)
+				return source.with(args)
+			end
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier.with({
-            filetypes = {
-              "javascript",
-              "typescript",
-              "css",
-              "scss",
-              "html",
-              "json",
-              "yaml",
-              "markdown",
-              "graphql",
-              "md",
-              "quarto",
-              "txt",
-            },
-          }),
-          null_ls.builtins.formatting.isort,
-          null_ls.builtins.formatting.csharpier,
-          null_ls.builtins.completion.spell,
+			local sql_formatter_config_file = os.getenv("HOME") .. "/.config/sql_formatter/sql_formatter.json"
 
-          -- referenced from http://github.com/milanglacier/nvim/blob/db850bbe400766932c1290c11d1e17672c324cbb/lua/conf/lsp_tools.lua#L135
-          source_wrapper({
-            null_ls.builtins.formatting.prettierd,
-            { ".prettirrc", ".prettirrc.json", ".prettirrc.yaml" },
-            filetypes = { "markdown.pandoc", "json", "markdown", "rmd", "yaml", "quarto" },
-          }),
-          source_wrapper({
-            null_ls.builtins.formatting.sql_formatter,
-            args = vim.fn.empty(vim.fn.glob(sql_formatter_config_file)) == 0
-                and { "--config", sql_formatter_config_file }
-                or nil,
-            -- This expression = 0 means this file exists.
-          }),
-        },
-      })
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier.with({
+						filetypes = {
+							"javascript",
+							"typescript",
+							"css",
+							"scss",
+							"html",
+							"json",
+							"yaml",
+							"markdown",
+							"graphql",
+							"md",
+							"quarto",
+							"txt",
+						},
+					}),
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.csharpier,
+					null_ls.builtins.completion.spell,
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+					-- referenced from http://github.com/milanglacier/nvim/blob/db850bbe400766932c1290c11d1e17672c324cbb/lua/conf/lsp_tools.lua#L135
+					source_wrapper({
+						null_ls.builtins.formatting.prettierd,
+						{ ".prettirrc", ".prettirrc.json", ".prettirrc.yaml" },
+						filetypes = { "markdown.pandoc", "json", "markdown", "rmd", "yaml", "quarto" },
+					}),
+					source_wrapper({
+						null_ls.builtins.formatting.sql_formatter,
+						args = vim.fn.empty(vim.fn.glob(sql_formatter_config_file)) == 0
+								and { "--config", sql_formatter_config_file }
+							or nil,
+						-- This expression = 0 means this file exists.
+					}),
+				},
+			})
 
-    opts = {
-      servers = {
-        tailwindcss = {},
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "ts_ls",
-          "tailwindcss",
-          "clangd",
-          "cmake",
-          "dockerls",
-          "jdtls",
-          "hls",
-          "html",
-          "marksman",
-          "omnisharp",
-          "csharp_ls",
-          "r_language_server",
-          "yamlls",
-          "jsonls",
-          "ltex",
-          "harper_ls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig.util")
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
 
-      -- Set up lspconfig.
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- lsp flags
-      local lsp_flags = {
-        allow_incremental_sync = true,
-        debounce_text_changes = 150,
-      }
+		opts = {
+			servers = {
+				tailwindcss = {},
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"ts_ls",
+					"tailwindcss",
+					"clangd",
+					"cmake",
+					"dockerls",
+					"jdtls",
+					"hls",
+					"html",
+					"marksman",
+					"omnisharp",
+					"csharp_ls",
+					"r_language_server",
+					"yamlls",
+					"jsonls",
+					"ltex",
+					"harper_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local util = require("lspconfig.util")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			-- Set up lspconfig.
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- lsp flags
+			local lsp_flags = {
+				allow_incremental_sync = true,
+				debounce_text_changes = 150,
+			}
 
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-      -- See https://github.com/neovim/neovim/issues/23291
-      -- disable lsp watcher.
-      -- Too lags on linux for python projects
-      -- because pyright and nvim both create too many watchers otherwise
-      if capabilities.workspace == nil then
-        capabilities.workspace = {}
-        capabilities.workspace.didChangeWatchedFiles = {}
-      end
-      capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-        cmd = { "pyright-langserver", "--stdio" },
-        settings = {
-          python = {
-            pythonPath = vim.g.python3_host_prog,
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              diagnosticMode = "workspace",
-            },
-          },
-        },
-        root_dir = function(fname)
-          return util.root_pattern(".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(
-            fname
-          ) or util.path.dirname(fname)
-        end,
-      })
+			-- See https://github.com/neovim/neovim/issues/23291
+			-- disable lsp watcher.
+			-- Too lags on linux for python projects
+			-- because pyright and nvim both create too many watchers otherwise
+			if capabilities.workspace == nil then
+				capabilities.workspace = {}
+				capabilities.workspace.didChangeWatchedFiles = {}
+			end
+			capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+				cmd = { "pyright-langserver", "--stdio" },
+				settings = {
+					python = {
+						pythonPath = vim.g.python3_host_prog,
+						analysis = {
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+						},
+					},
+				},
+				root_dir = function(fname)
+					return util.root_pattern(".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(
+						fname
+					) or util.path.dirname(fname)
+				end,
+			})
 
-      lspconfig.cmake.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.dockerls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.jdtls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.hls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.hls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.marksman.setup({
-        capabilities = capabilities,
-        filetypes = { "markdown", "quarto" },
-        root_dir = util.root_pattern(".git", ".marksman.toml", "_quarto.yml"),
-      })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.omnisharp.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+				filetypes = { "markdown", "quarto" },
+				root_dir = util.root_pattern(".git", ".marksman.toml", "_quarto.yml"),
+			})
 
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.r_language_server.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-        settings = {
-          yaml = {
-            schemaStore = {
-              enable = true,
-              url = "",
-            },
-          },
-        },
-      })
+			lspconfig.r_language_server.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
 
-      lspconfig.ltex.setup({
-        filetypes = { "quarto" },
-        settings = {
-          ltex = {
-            checkFrequency = "save",
-            language = "en",
-            additionalRules = {
-              languageModel = "~/ltex-models/ngrams",
-            },
-          },
-        },
-      })
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+				settings = {
+					yaml = {
+						schemaStore = {
+							enable = true,
+							url = "",
+						},
+					},
+				},
+			})
 
-      lspconfig.harper_ls.setup({
-        capabilities = capabilities,
-        filetypes = {
-          "c",
-          "cpp",
-          "cs",
-          "gitcommit",
-          "go",
-          "html",
-          "java",
-          "javascript",
-          "lua",
-          "markdown",
-          "nix",
-          "python",
-          "ruby",
-          "rust",
-          "swift",
-          "toml",
-          "typescript",
-          "typescriptreact",
-          "haskell",
-          "cmake",
-          "typst",
-          "php",
-          "dart",
-        },
-        settings = {
-          ["harper-ls"] = {
-            markdown = {
-              ignore_link_title = true,
-            },
-          },
-        },
-      })
+			lspconfig.ltex.setup({
+				filetypes = { "quarto" },
+				settings = {
+					ltex = {
+						checkFrequency = "save",
+						language = "en",
+						additionalRules = {
+							languageModel = "~/ltex-models/ngrams",
+						},
+					},
+				},
+			})
 
-      lspconfig.sqls.setup({
-        capabilities = capabilities,
-        flags = lsp_flags,
-      })
+			lspconfig.harper_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"c",
+					"cpp",
+					"cs",
+					"gitcommit",
+					"go",
+					"html",
+					"java",
+					"javascript",
+					"lua",
+					"markdown",
+					"nix",
+					"python",
+					"ruby",
+					"rust",
+					"swift",
+					"toml",
+					"typescript",
+					"typescriptreact",
+					"haskell",
+					"cmake",
+					"typst",
+					"php",
+					"dart",
+				},
+				settings = {
+					["harper-ls"] = {
+						markdown = {
+							ignore_link_title = true,
+						},
+					},
+				},
+			})
 
-      vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-    servers = {
-      ltex = {
-        autostart = false,
-        filetypes = {},
-        settings = {
-          ltex = {
-            checkFrequency = "save",
-            language = "en-US",
-            additionalRules = {
-              languageModel = "~/.local/share/ngrams/",
-              -- enablePickyRules = true,
-              -- motherTongue = "de",
-            },
-            disabledRules = {
-              ["en"] = { "MORFOLOGIK_RULE_EN" },
-              ["en-GB"] = { "MORFOLOGIK_RULE_EN_GB" },
-              ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
-              ["de"] = { "MORFOLOGIK_RULE_DE_DE" },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    -- For lsp features in code cells / embedded code
-    "jmbuhr/otter.nvim",
-    dev = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
-      },
-    },
-    opts = {
-      verbose = {
-        no_code_found = false,
-      },
-    },
-  },
+			lspconfig.sqls.setup({
+				capabilities = capabilities,
+				flags = lsp_flags,
+			})
+
+			vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+		servers = {
+			ltex = {
+				autostart = false,
+				filetypes = {},
+				settings = {
+					ltex = {
+						checkFrequency = "save",
+						language = "en-US",
+						additionalRules = {
+							languageModel = "~/.local/share/ngrams/",
+							-- enablePickyRules = true,
+							-- motherTongue = "de",
+						},
+						disabledRules = {
+							["en"] = { "MORFOLOGIK_RULE_EN" },
+							["en-GB"] = { "MORFOLOGIK_RULE_EN_GB" },
+							["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+							["de"] = { "MORFOLOGIK_RULE_DE_DE" },
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		-- For lsp features in code cells / embedded code
+		"jmbuhr/otter.nvim",
+		dev = false,
+		dependencies = {
+			{
+				"neovim/nvim-lspconfig",
+				"nvim-treesitter/nvim-treesitter",
+			},
+		},
+		opts = {
+			verbose = {
+				no_code_found = false,
+			},
+		},
+	},
 }
