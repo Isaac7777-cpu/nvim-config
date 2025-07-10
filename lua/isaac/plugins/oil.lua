e443f1703eff161ed3aa34dc782a86fe68f80e9c
Isaac Leong <isaacleong23367@gmail.com>
2025-07-10 16:46:47 +0800

    Updated oil

--- lua/isaac/plugins/oil.lua
+++ lua/isaac/plugins/oil.lua
@@ -1,11 +1,29 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-		lazy = false,
-	},
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-as>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-ah>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-at>"] = { "actions.select", opts = { tab = true } },
+      ["<C-ap>"] = "actions.preview",
+      ["<C-ac>"] = { "actions.close", mode = "n" },
+      ["<C-al>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+  },
 }
