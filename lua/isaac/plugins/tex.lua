23f77420a84ce6f7ea164863e1224b86692a3445
Isaac Leong <isaacleong23367@gmail.com>
2025-09-17 11:38:55 +1000

    Added Skim for Markdown Previewing
    I think this is actually very good. It achieves most of the
    functionality that I have for overleaf so I guess it is much better than
    I thought.

--- lua/isaac/plugins/tex.lua
+++ lua/isaac/plugins/tex.lua
@@ -5,9 +5,11 @@ return {
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
 			-- VimTeX configuration goes here, e.g.
-      -- I recommend using tdf to view the pdf right in the terminal
-			vim.g.vimtex_view_method = "general" -- Disable PDF viewer
-			vim.g.vimtex_view_general_viewer = ""
+			-- I recommend using tdf to view the pdf right in the terminal
+			vim.g.vimtex_view_method = "skim" -- Disable PDF viewer
+			-- vim.g.vimtex_view_general_viewer = ""
+			vim.g.vimtex_quickfix_open_on_warning = 0
+			vim.g.vimtex_quickfix_mode = 0
 		end,
 	},
 }
