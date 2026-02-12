b22e7b4338e304f20db1bb0d72767753ea19392f
Isaac Leong <isaacleong23367@gmail.com>
2026-02-12 13:35:59 +1100

    Clean-up: Removed unused code in the quarto mapping file

--- lua/isaac/quarto-mappings.lua
+++ lua/isaac/quarto-mappings.lua
@@ -40,104 +40,12 @@ imap(";", ";<c-g>u")
 
 nmap("Q", "<Nop>")
 
---- Send code to terminal with vim-slime
---- If an R terminal has been opend, this is in r_mode
---- and will handle python code via reticulate when sent
---- from a python chunk.
---- TODO: incorpoarate this into quarto-nvim plugin
---- such that QuartoRun functions get the same capabilities
---- TODO: figure out bracketed paste for reticulate python repl.
--- local function send_cell()
--- 	if vim.b["quarto_is_r_mode"] == nil then
--- 		vim.fn["slime#send_cell"]()
--- 		return
--- 	end
--- 	if vim.b["quarto_is_r_mode"] == true then
--- 		vim.g.slime_python_ipython = 0
--- 		local is_python = require("otter.tools.functions").is_otter_language_context("python")
--- 		if is_python and not vim.b["reticulate_running"] then
--- 			vim.fn["slime#send"]("reticulate::repl_python()" .. "\r")
--- 			vim.b["reticulate_running"] = true
--- 		end
--- 		if not is_python and vim.b["reticulate_running"] then
--- 			vim.fn["slime#send"]("exit" .. "\r")
--- 			vim.b["reticulate_running"] = false
--- 		end
--- 		vim.fn["slime#send_cell"]()
--- 	end
--- end
-
---- Send code to terminal with vim-slime
---- If an R terminal has been opend, this is in r_mode
---- and will handle python code via reticulate when sent
---- from a python chunk.
--- local slime_send_region_cmd = ":<C-u>call slime#send_op(visualmode(), 1)<CR>"
--- slime_send_region_cmd = vim.api.nvim_replace_termcodes(slime_send_region_cmd, true, false, true)
--- local function send_region()
--- 	-- if filetyps is not quarto, just send_region
--- 	if vim.bo.filetype ~= "quarto" or vim.b["quarto_is_r_mode"] == nil then
--- 		vim.cmd("normal" .. slime_send_region_cmd)
--- 		return
--- 	end
--- 	if vim.b["quarto_is_r_mode"] == true then
--- 		vim.g.slime_python_ipython = 0
--- 		local is_python = require("otter.tools.functions").is_otter_language_context("python")
--- 		if is_python and not vim.b["reticulate_running"] then
--- 			vim.fn["slime#send"]("reticulate::repl_python()" .. "\r")
--- 			vim.b["reticulate_running"] = true
--- 		end
--- 		if not is_python and vim.b["reticulate_running"] then
--- 			vim.fn["slime#send"]("exit" .. "\r")
--- 			vim.b["reticulate_running"] = false
--- 		end
--- 		vim.cmd("normal" .. slime_send_region_cmd)
--- 	end
--- end
-
--- send code with ctrl+Enter
--- just like in e.g. RStudio
--- needs kitty (or other terminal) config:
--- map shift+enter send_text all \x1b[13;2u
--- map ctrl+enter send_text all \x1b[13;5u
--- nmap("<c-cr>", quarto_runner.run_cell(true))
--- nmap("<s-cr>", quarto_runner.run_cell(true))
--- imap("<c-cr>", quarto_runner.run_cell(true))
--- imap("<s-cr>", quarto_runner.run_cell(true))
-
---- Show R dataframe in the browser
--- might not use what you think should be your default web browser
--- because it is a plain html file, not a link
--- see https://askubuntu.com/a/864698 for places to look for
--- local function show_r_table()
--- 	local node = vim.treesitter.get_node({ ignore_injections = false })
--- 	assert(node, "no symbol found under cursor")
--- 	local text = vim.treesitter.get_node_text(node, 0)
--- 	local cmd = [[call slime#send("DT::datatable(]] .. text .. [[)" . "\r")]]
--- 	vim.cmd(cmd)
--- end
-
 -- keep selection after indent/dedent
 vmap(">", ">gv")
 vmap("<", "<gv")
 
 -- center after search and jumps
 nmap("n", "nzz")
-
--- -- move between splits and tabs
--- nmap('<c-h>', '<c-w>h')
--- nmap('<c-l>', '<c-w>l')
--- nmap('<c-j>', '<c-w>j')
--- nmap('<c-k>', '<c-w>k')
--- nmap('H', '<cmd>tabprevious<cr>')
--- nmap('L', '<cmd>tabnext<cr>')
-
-local function toggle_light_dark_theme()
-	if vim.o.background == "light" then
-		vim.o.background = "dark"
-	else
-		vim.o.background = "light"
-	end
-end
 
 local is_code_chunk = function()
 	local current, _ = require("otter.keeper").get_current_language_context()
@@ -302,6 +210,8 @@ vim.keymap.set("n", "<leader>os", get_otter_symbols_lang, { desc = "otter [s]ymb
 -- normal mode with <leader>
 wk.add({
 	{
+		{ "<leader>w", group = "sessions", icon = { icon = "󰆦", color = "cyan" } },
+		{ "<leader>x", group = "trouble" },
 		-- { "<leader><cr>", send_cell, desc = "run code cell" },
 		{ "<leader>c", group = "[c]ode / [c]ell / [c]hunk" },
 		{ "<leader>sit", new_terminal_ipython, desc = "new [i]python terminal" },
