4295727e46718e467962e2c9ee1415947de339b5
Isaac Leong <isaacleong23367@gmail.com>
2026-02-12 13:21:14 +1100

    Feat: Implemented Range formatting
    This is useful when I am writing codes in a large codespace where there isn't really a formatting standard and I want to avoid formatting other's codes.

--- lua/isaac/mappings.lua
+++ lua/isaac/mappings.lua
@@ -50,9 +50,21 @@ vim.keymap.set(
 )
 
 -- Set <leader>gf to use conform.nvim to format
-vim.keymap.set("n", "<leader>gf", function()
-	require("conform").format({ lsp_fallback = true })
-end, { desc = "Format buffer with conform.nvim" })
+vim.keymap.set({ "n", "x" }, "<leader>gf", function()
+	require("conform").format({ async = false, lsp_fallback = true }, function(err)
+		if not err then
+			local mode = vim.api.nvim_get_mode().mode
+			if vim.startswith(string.lower(mode), "v") then
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+			end
+
+			-- Reset horizontal scroll WITHOUT moving cursor
+			local view = vim.fn.winsaveview()
+			view.leftcol = 0
+			vim.fn.winrestview(view)
+		end
+	end)
+end, { desc = "Format code" })
 
 -- Custom setup for wrapping. I have to write a lot of latex and hence find
 -- it would be useful if it can automatically wrap the text for me.
