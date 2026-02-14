a57c4439678e66f993775877a1b1e5428d9e9b82
Isaac Leong <isaacleong23367@gmail.com>
2026-02-15 10:50:24 +1100

    Updated Markdown Snippets as I am using Neovim Obsidian to write notes

---
+++ lua/isaac/snippets/markdown.lua
@@ -0,0 +1,12 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+
+return {
+	-- arrows
+	s("->", fmt("$\\rightarrow$ ", {}), {}),
+	s("<-", fmt("$\\leftarrow$ ", {}), {}),
+}
